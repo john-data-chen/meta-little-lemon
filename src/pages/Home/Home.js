4ff84ae8ce12bc342a5cd2820f0279cd9461b08d
John Chen <john.data.chen@gmail.com>
2024-10-23 11:07:31 +0800

    Init about in homepage

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import About from "../../components/Home/About/About";
+
+const Home = () => {
+  return (
+    <>
+      <About />
+    </>
+  );
+};
+export default Home;
